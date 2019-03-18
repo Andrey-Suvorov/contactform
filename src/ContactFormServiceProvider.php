8956d986a6086d406a941ac7dd4f4b46445d4516
AndreySuvorov <suvorov.andrey.work@gmail.com>
2019-03-18 11:58:18 +0200

    add publish for migration

--- src/ContactFormServiceProvider.php
+++ src/ContactFormServiceProvider.php
@@ -11,6 +11,10 @@
             $this->loadRoutesFrom(__DIR__.'/routes/web.php');
             $this->loadViewsFrom(__DIR__.'/resources/views', 'contactform');
             $this->loadMigrationsFrom(__DIR__.'/Database/migrations');
+
+            $this->publishes([
+                __DIR__.'/../database/migrations/' => database_path('migrations')
+            ], 'migrations');
         }
 
         public function register()
